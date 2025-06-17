7e96693c3229158cd0439244b216c435d8fbb496
biswashdaiii <biswashghimire97@gmail.com>
2025-06-17 18:00:48 +0545

    started admin api

--- controllers/admin_controller.js
+++ controllers/admin_controller.js
@@ -4,8 +4,11 @@
 //Api for add doct
 const addDoctor=async(req,res)=>{
     try {
-        
+        const {name,email,password,speciality,degree,about,fees,address}=req.body
+        const imageFile=req.file
+        console.log()
     } catch (error) {
         
     }
-}
+}   
+export {addDoctor}
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 import express from "express";
-import mongoose from "mongoose";
+import mongoose, { connect } from "mongoose";
 import cors from "cors";
 import authRoutes from "./routes/userRoutes.js";
 import { registerUser, loginUser, getUsers } from "./controllers/userController.js";
+import adminRouter from "./routes/adminRoute.js";
+
+//app config
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+//middlewares
 app.use(cors());
 app.use(express.json());
 
@@ -16,7 +22,11 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
+//api endpoints
+app.use('/api/admin',adminRouter)
+
+
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
