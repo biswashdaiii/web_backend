5f23ce567a5465dc41b3228445b352fcbc463a28
biswashdaiii <biswashghimire97@gmail.com>
2025-06-25 20:05:51 +0545

    api done

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ import { registerUser, loginUser, getUsers } from "./controllers/userController.
 import adminRouter from "./routes/adminRoute.js";
 import 'dotenv/config'
 import { connectDB } from "./config/mongodb.js";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from"path"
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 //app config
 connectDB()
@@ -16,6 +22,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 //api endpoint
 
